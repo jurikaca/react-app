64e903f1e3b29f57d6594435083b005f18c4fe0e
Inis Kryeziu <inis.kryeziu2000@gmail.com>
2023-05-05 17:59:32 +0200

    delete message, get response from mock handler

--- src/components/exercises/App1/redux/slices/messageSlice.js
+++ src/components/exercises/App1/redux/slices/messageSlice.js
@@ -1,32 +1,59 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import "babel-polyfill";
 
-export const fetchMessages = createAsyncThunk('messages/fetchMessages', async () =>{
-    const response = await fetch('list-messages.php').then(response => response.json());
+export const fetchMessages = createAsyncThunk(
+  "messages/fetchMessages",
+  async () => {
+    const response = await fetch("list-messages.php").then((response) =>
+      response.json()
+    );
     return response.messages;
-})
+  }
+);
+
+export const deleteMessageThunk = createAsyncThunk(
+  "messages/deleteMessages",
+  async (data) => {
+    const response = await fetch("delete-message", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
+    return {
+      response,
+      request: data,
+    };
+  }
+);
 
 export const messageSlice = createSlice({
-    name: 'message',
-    initialState: {
-        status: 'idle',
-        data: [],
+  name: "message",
+  initialState: {
+    status: "idle",
+    data: [],
+  },
+  reducers: {
+    sendMessage: (state, action) => {
+      state.data.push(action.payload);
     },
-    reducers: {
-        sendMessage: (state, action) => {
-            state.data.push(action.payload);
-        },
-        deleteMessage: (state, action) => {
-            state.data = state.data.filter((message) => message.id !== action.payload);
-        }
-    },
-    extraReducers(builder){
-        builder.addCase(fetchMessages.fulfilled, (state, action) => {
-            state.data = action.payload;
-        })
-    }
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchMessages.fulfilled, (state, action) => {
+        state.data = action.payload;
+      })
+      .addCase(deleteMessageThunk.fulfilled, (state, action) => {
+        state.data = state.data.filter(
+          (message) => message.id !== action.payload.request.id
+        );
+      })
+
+      .addCase(deleteMessageThunk.rejected, (state, action) => {
+        console.log("extra reducers rejected", action);
+      });
+  },
 });
 
-export const { sendMessage, deleteMessage } = messageSlice.actions;
+export const { sendMessage } = messageSlice.actions;
 
 export default messageSlice.reducer;
