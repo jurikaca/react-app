9b665c41f7e0dccc80d7e42dd3a4e4fae01ff017
Inis Kryeziu <inis.kryeziu2000@gmail.com>
2023-05-10 16:21:52 +0200

    fix small issues

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -53,7 +53,7 @@ export const handlers = [
   rest.post("/contact", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ success: true }));
   }),
-  rest.post("/delete-contact", (req, res, ctx) => {
+  rest.delete("contact/:contactId", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ success: true }));
   }),
   // rest.get('/user', (req, res, ctx) => {
