ae2856d1984b3818ab4dd3ec0d4032263d337d6a
Inis Kryeziu <inis.kryeziu2000@gmail.com>
2023-05-12 12:49:52 +0200

    fix and finish small problem

--- src/components/exercises/App1/left/Messages.js
+++ src/components/exercises/App1/left/Messages.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
 import Message from "./Message";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function Messages() {
+  const messages = useSelector((selector) => selector.message.data);
 
-    const messages = useSelector(selector => selector.message.messages);
-
-  console.log('rendered messages component');
+  console.log("rendered messages component");
   return (
     <section className="messages">
-        { messages && messages[0] ? <ul>
-            {messages.map(message =>
-              <Message key={message.id} message={message}/>
-            )}
-          </ul> : <p>No messages yet</p> }
+      {messages && messages[0] ? (
+        <ul>
+          {messages.map((message) => (
+            <Message key={message.id} message={message} />
+          ))}
+        </ul>
+      ) : (
+        <p>No messages yet</p>
+      )}
     </section>
   );
 }
